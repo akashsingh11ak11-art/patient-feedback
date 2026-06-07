46d7f8a6628b8876ba712eae8cb2701d3420f8ec
akashsingh11ak11-art <akashsingh11ak11@gmail.com>
2026-06-07 13:46:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
-// frontend/config.js
-
 const APP_CONFIG = {
-    // Jis doctor ko demo dena hai, uska data yahan daalo:
-    clinicName: "City Dental Care",  
-    doctorName: "Dr. A.K. Sharma",
-    
-    // GMB link abhi demo ke liye khali chhod sakte ho
-    gmbLink: "https://search.google.com/local/writereview?placeid=YOUR_PLACE_ID",
-    
-    // Backend Server URL (Localhost testing ke liye)
-    backendUrl: "http://localhost:5000"
+    clinicName: "Rajeshwar Hospital",
+    doctorName: "Rajeshwar Hospital",
+    gmbLink: "https://search.google.com/local/writereview?placeid= ChIJex73_5VY7TkRjBkSD-0niRk&rating=5",
+    backendUrl: " https://stopper-gnarly-object.ngrok-free.dev"
 };
